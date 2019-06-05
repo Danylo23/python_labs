176cf1a2d38d8be619a3680ab697fc74df6a15d9
Danylo23 <44833735+Danylo23@users.noreply.github.com>
2019-06-05 23:21:04 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
-from lab10_python import Message
+import re
 
 
-def main():
-    message1 = Message('Message1', "Jimmy", 25, "Amy", "Iphone", "21:30")
-    message2 = Message('Message2', "Jimmy")
-    message3 = Message('Message3', "Jimmy", 25, "Amy")
-    message1.MessageInfo()
-    message2.MessageInfo()
-    message3.MessageInfo()
-
-
-if __name__ == '__main__':
-  main()
+with open("server.log.2018-04-05") as file:
+    for line in file:
+        line = file.readline()
+        result = re.search("WFLYSRV"
+                           ".*OS signal", line)
+        if result:
+            print(line)
